009f76f0bc266bb12f0570d10eda9dd00df588fb
Ahmed Mujtaba <mujtabachang@gmail.com>
2021-12-14 17:55:58 +0500

    Timer to look for exercises list to hide

--- src/assets/js/hide-libraries.js
+++ src/assets/js/hide-libraries.js
@@ -1,6 +1,8 @@
 
 H5P.jQuery(document).ready(function () {
     var hideExercises = [];
+    var exercisesFound = false;
+    var hideExercisesTimer = null;
 
     // This script forces to click on Check button when there is click on the Finish (For QuestionSet Exercises)
 
@@ -16,6 +18,29 @@ H5P.jQuery(document).ready(function () {
                 if (data.result.length > 0) {
                     if (JSON.parse(data.result).hideExercises) {
                         hideExercises = JSON.parse(data.result).hideExercises;
+
+                        // Start the timer
+                        hideExercisesTimer = setInterval(function () {
+
+                            var container = document.getElementsByClassName("content-type-list")[0];
+
+                            if (container) {
+                                let listOfExercises = container.children[0];
+                                // Loop through listOfExercises and hide the ones we want
+                                for (let i = 0; i < listOfExercises.children.length; i++) {
+                                    let exercise = listOfExercises.children[i];
+                                    for (let j = 0; j < hideExercises.length; j++) {
+                                        if (exercise.id == hideExercises[j]) {
+                                            exercise.style.display = "none";
+                                        }
+                                    }
+                                }
+                                console.log("Exercises found, clearing timer");
+                                clearInterval(hideExercisesTimer);
+                            }
+
+
+                        }, 500);
                     }
                 }
             }
@@ -24,44 +49,44 @@ H5P.jQuery(document).ready(function () {
             });
 
 
-        // Attach a mutation observer to check for any DOM changes and see if we have any check and finish buttons appearing
-        // Callback function when changes occurs
-        function callback(mutationRecord, observer) {
+        // // Attach a mutation observer to check for any DOM changes and see if we have any check and finish buttons appearing
+        // // Callback function when changes occurs
+        // function callback(mutationRecord, observer) {
 
-            //hideExercises = ["h5p-interactivevideo", "h5p-coursepresentation"];
+        //     //hideExercises = ["h5p-interactivevideo", "h5p-coursepresentation"];
 
-            var container = document.getElementsByClassName("content-type-list")[0];
+        //     var container = document.getElementsByClassName("content-type-list")[0];
 
-            if (container) {
-                let listOfExercises = container.children[0];
-                // Loop through listOfExercises and hide the ones we want
-                for (let i = 0; i < listOfExercises.children.length; i++) {
-                    let exercise = listOfExercises.children[i];
-                    for (let j = 0; j < hideExercises.length; j++) {
-                        if (exercise.id == hideExercises[j]) {
-                            exercise.style.display = "none";
-                        }
-                    }
-                }
+        //     if (container) {
+        //         let listOfExercises = container.children[0];
+        //         // Loop through listOfExercises and hide the ones we want
+        //         for (let i = 0; i < listOfExercises.children.length; i++) {
+        //             let exercise = listOfExercises.children[i];
+        //             for (let j = 0; j < hideExercises.length; j++) {
+        //                 if (exercise.id == hideExercises[j]) {
+        //                     exercise.style.display = "none";
+        //                 }
+        //             }
+        //         }
 
-            }
+        //     }
 
 
-        }
+        // }
 
-        // Create a new instance of MutationObserver with callback in params
-        const observer = new MutationObserver(callback);
+        // // Create a new instance of MutationObserver with callback in params
+        // const observer = new MutationObserver(callback);
 
-        // Setup config
-        const config = {
-            childList: true,
-            attributes: true,
-            characterData: true,
-            subtree: true,
-        };
+        // // Setup config
+        // const config = {
+        //     childList: true,
+        //     attributes: true,
+        //     characterData: true,
+        //     subtree: true,
+        // };
 
-        let container = document.documentElement || document.body;
-        observer.observe(container, config);
+        // let container = document.documentElement || document.body;
+        // observer.observe(container, config);
 
     }
 });
